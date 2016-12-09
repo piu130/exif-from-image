7ddf96fa06f407321a91a5584c40cab2814fb92b
Ramon Boss <ramon.boss@outlook.com>
2016-12-09 21:02:28 +0100

    added some exif variables

---
+++ exif.js
@@ -0,0 +1,39 @@
+/**
+ * Indicates start of a JPEG
+ * @type {number}
+ */
+const jpegStartNumber = 0xFFD8;
+
+/**
+ * Indicates start of the EXIF attribute
+ * @type {number}
+ */
+const exifStartNumber = 0xFFE1;
+
+/**
+ * Hex value for 'Exif'
+ * @type {number}
+ */
+const exifString = 0x45786966; // Exif
+
+/**
+ * Hex value for 'II'. This indicates little endian.
+ * @type {number}
+ */
+const littleEndianIndicator = 0x4949; // II
+
+/**
+ * Hex value for 'MM'. This indicates big endian.
+ * @type {number}
+ */
+const bigEndianIndicator = 0x4D4D; // MM
+
+/**
+ *
+ * @type {[number]}
+ */
+const exifPointers = [
+  0x8769, // ExifIFDPointer
+  0x8825, // GPSInfoIFDPointer
+  0xA005 // InteroperabilityIFDPointer
+];
